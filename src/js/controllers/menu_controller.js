ce818dcf1418c253abcef6a0473fc043199e9c4f
prikhodkin <info@prikhodkin.com>
2020-09-21 10:22:10 +0500

    Добавляет футер

--- src/js/controllers/menu_controller.js
+++ src/js/controllers/menu_controller.js
@@ -2,7 +2,11 @@ import { Controller } from "stimulus";
 import {UNIT, ADD_WIDTH} from "../../js/util";
 
 export default class extends Controller {
-  static targets = [ `item`, `trigger` ]
+  static targets = [ `item`, `trigger`, `list` ]
+
+  connect() {
+    this.hideOnHover();
+  }
 
   changeBurger() {
     const inner = this.triggerTarget.querySelector(`.burger__inner`);
@@ -14,6 +18,22 @@ export default class extends Controller {
     this.changeBurger();
     const menu = document.querySelector(`.menu__list`);
     menu.classList.toggle(`menu__list--active`);
+  }
+
+  hideOnHover() {
+    this.listTarget.addEventListener(`mouseover`, () => {
+      const active = this.listTarget.querySelector(`.mega-menu__item--active`);
+      if (active) {
+        active.classList.remove(`mega-menu__item--active`);
+      }
+    })
+
+    this.listTarget.addEventListener(`mouseout`, () => {
+      const active = this.listTarget.querySelector(`.mega-menu__item`);
+      if (active) {
+        active.classList.add(`mega-menu__item--active`);
+      }
+    })
   }
 
 }
