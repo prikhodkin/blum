1fcf27d6acabbd23b14728a57092ac7a1bcc2d38
prikhodkin <info@prikhodkin.com>
2020-09-25 10:05:43 +0500

    Добавляет блоки отзывы и видео

--- src/js/general.js
+++ src/js/general.js
@@ -3,8 +3,11 @@ import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 import Tabs from "%modules%/tabs/tabs"
-import {$, $$} from "./util";
+import {$$} from "./util";
+import $ from "jquery";
+global.jQuery = $;
 
+require ("@fancyapps/fancybox/dist/jquery.fancybox");
 
 Turbolinks.start();
 
@@ -12,3 +15,11 @@ const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
 
+const tabs = $$(`[data-tabs]`);
+console.log(tabs)
+
+
+tabs.map((item) => new Tabs(item));
+$('[data-fancybox]').fancybox({
+  protect: true
+});
