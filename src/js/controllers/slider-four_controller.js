88cddec6d0419badde9b341f58d35183c2cb4b22
prikhodkin <info@prikhodkin.com>
2020-09-25 19:34:07 +0500

    Добавляет блок патентов

---
+++ src/js/controllers/slider-four_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "stimulus";
+import { tns } from "tiny-slider/src/tiny-slider"
+
+export default class extends Controller {
+  static targets = [ "prev", "next", "list" ]
+
+  connect() {
+    this.init();
+  }
+
+  init() {
+    const sliderFive = tns({
+      container: this.listTarget,
+      items: 1,
+      controls: true,
+      nav: false,
+      loop: false,
+      swipeAngle: false,
+      speed: 400,
+      nextButton: this.nextTarget,
+      prevButton: this.prevTarget,
+      responsive: {
+        600: {
+          items: 2
+        },
+        900: {
+          items: 3
+        },
+        1200: {
+          items: 4
+        }
+      }
+    });
+  }
+
+}
