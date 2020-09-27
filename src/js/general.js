330ff85b83d15880d5759b030495981f628a62bb
prikhodkin <info@prikhodkin.com>
2020-09-27 15:00:29 +0500

    Добавляет блок обратной связи

--- src/js/general.js
+++ src/js/general.js
@@ -5,6 +5,9 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 import Tabs from "%modules%/tabs/tabs"
 import {$$} from "./util";
 import $ from "jquery";
+import intlTelInput from 'intl-tel-input';
+
+
 global.jQuery = $;
 
 require ("@fancyapps/fancybox/dist/jquery.fancybox");
@@ -16,10 +19,32 @@ const context = require.context("./controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
 
 const tabs = $$(`[data-tabs]`);
-console.log(tabs)
+const input = document.querySelector("#phone");
+
+
+intlTelInput(input, {
+  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.5/js/utils.js",
+});
 
 
 tabs.map((item) => new Tabs(item));
 $('[data-fancybox]').fancybox({
   protect: true
+});
+
+let inputs = document.querySelectorAll('.file__input');
+Array.prototype.forEach.call(inputs, function (input) {
+  let label = input.nextElementSibling,
+    labelVal = label.querySelector('.file__text').innerText;
+
+  input.addEventListener('change', function (e) {
+    let countFiles = '';
+    if (this.files && this.files.length >= 1)
+      countFiles = this.files.length;
+
+    if (countFiles)
+      label.querySelector('.file__text').innerText = 'Выбрано файлов: ' + countFiles;
+    else
+      label.querySelector('.file__text').innerText = labelVal;
+  });
 });
